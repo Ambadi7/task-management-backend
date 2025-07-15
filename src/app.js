2f83ea38ebac19f5450b5501af3893c67205d2fe
Ambadi7 <ambadikr24@gmail.com>
2025-07-16 01:25:38 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,15 @@ import crypto from "crypto"
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+    {
+  origin: [
+    'http://localhost:3000', // for local dev
+    'https://task-management-frontend-sgq8.vercel.app/', // replace with your actual deployed URL
+  ],
+  credentials: true, // if you're using cookies or auth headers
+}
+))
 app.use(morgan())
 app.use(express.json())
 app.use(cookieParser())
