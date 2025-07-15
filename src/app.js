bd31c90431a6304de95c2842221216b14c82d3af
Ambadi7 <ambadikr24@gmail.com>
2025-07-16 00:11:17 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/authRoutes.js"
+import taskRoutes from "./routes/taskRoutes.js"
+import crypto from "crypto"
+
+const app = express()
+
+app.use(cors())
+app.use(morgan())
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes)
+app.use("/api/tasks",taskRoutes)
+
+//Generate secret key
+// const key = crypto.randomBytes(64).toString("hex")
+// console.log(key)
+
+app.get("/",(req,res)=>{
+    res.send("<h1>helloworld</h1>")
+})
+
+export default app
